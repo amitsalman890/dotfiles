5b9de261a403386e9e40da376f77a90ea1d546dc
Amit-proj <Amitsalman80@gmail.com>
2025-05-26 13:01:40 +0300

    add hardtime

--- nvim/.config/nvim/lua/plugins/functionality.lua
+++ nvim/.config/nvim/lua/plugins/functionality.lua
@@ -162,6 +162,19 @@ local M = {
       vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
     end,
   },
+  {
+    'm4xshen/hardtime.nvim',
+    lazy = false,
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {
+      disabled_keys = {
+        ['<Up>'] = false, -- Allow <Up> key
+        ['<Down>'] = false, -- Allow <Down> key
+        ['<Left>'] = false, -- Allow <Left> key
+        ['<Right>'] = false, -- Allow <Right> key
+      },
+    },
+  },
 }
 
 return M
