a3a82f545bf6447b71c409b4d6d06740d754784c
amitsalman <79787072+amitsalman890@users.noreply.github.com>
2025-07-17 12:08:39 +0300

    chore/ update k8s and sync mosh branch (#8)
    * moshe-17-7
    
    * moshe-17-7

--- nvim/.config/nvim/lua/plugins/octo.lua
+++ nvim/.config/nvim/lua/plugins/octo.lua
@@ -1,9 +1,14 @@
+local octo_string = 'Octo: '
 return {
   'pwntester/octo.nvim',
-  keys = { { '<leader>o', '<cmd>Octo<cr>' } },
+  keys = { { '<leader>o', '<cmd>Octo<cr>', desc = 'Octo menu' } },
   cmd = 'Octo',
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim',
+  --   'nvim-telescope/telescope.nvim',
+  -- },
   opts = {
-    use_local_fs = false, -- use local files on right side of reviews
+    use_local_fs = true, -- use local files on right side of reviews
     enable_builtin = true, -- shows a list of builtin actions when no action is provided
     default_merge_method = 'squash',
     default_delete_branch = true, -- whether to delete branch when merging pull request with either `Octo pr merge` or from picker (can be overridden with `delete`/`nodelete` argument to `Octo pr merge`)
@@ -15,11 +20,17 @@ return {
     pull_requests = {
       use_branch_name_as_title = true, -- sets branch name to be the name for the PR
     },
+    mappings = {
+      notification = {
+        read = { lhs = '<C-r>' },
+        done = { lhs = '<C-d>' },
+        unsubscribe = { lhs = '<C-u>' },
+      },
+    },
   },
   config = function(_, opts)
     local octo_config = require 'octo.config'
     local default_opts = octo_config.get_default_values()
-    local octo_string = 'Octo: '
     for _, actions in pairs(default_opts.mappings) do
       for _, details in pairs(actions) do
         if details.desc then
@@ -30,7 +41,8 @@ return {
     -- require('octo').setup(opts)
     -- setup octo and merge opts with default opts
     require('octo').setup(vim.tbl_deep_extend('force', default_opts, opts))
-
+  end,
+  init = function()
     -- keymap <leader>gk to open fzf.lua's keymaps with a ready prompt for
     -- "Octo: " to see all octo keymaps when you press <leader>gk
     vim.keymap.set('n', '<leader>gk', function()
